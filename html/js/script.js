103aad0178374a12638c1e36c433a1e08a2563d7
Mark Poelstra <mark.poelstra@clockwork.nl>
2013-07-25 10:18:46 +0200

    diverse styling issues

--- html/js/script.js
+++ html/js/script.js
@@ -1,21 +1,68 @@
 $(document).ready(function() {
 
-    $('.fancybox').fancybox();
-
     NA.controller.initialize(function() {
         if(window.location.hash) {
             NA.controller.goToPage(window.location.hash);
         }
 
+        //all popups
+        $('.fancybox').fancybox();
 
+        //popup submit button
+        $('.popup').find('a.submit').on('click', function(e) {
+            e.preventDefault();
+            var type = $(this).closest('.popup').attr('id');
+            switch (type) {
+                case 'nieuw':
+                    alert('nummer toevoegen aan flexslider en deze ws her-initialiseren');
+                    break;
+                case 'melding':
+                    alert('melding opslaan en tonen bij de studiezaal pagina');
+                    break;
+            }
+            $.fancybox.close();
+        });
 
+        //show change bar
+        $('#balie').find('a.nr').on('click', function(e) {
+            e.preventDefault();
+            $(this).closest('.page').find('a.nr.selected').removeClass('selected');
+            $(this).addClass('selected');
+            var nr = $(this).find('span').html();
+            var changeNav = $(this).closest('.page').find('nav.change');
+            var actionsNav = $(this).closest('.page').find('nav.actions');
+            changeNav.find('.nummer').html(nr);
+
+            actionsNav.hide();
+            changeNav.show();
+        });
+
+        //delete pasnr
+        $('#balie').find('a.delete').on('click', function(e) {
+            e.preventDefault();
+            var selectedPas = $(this).closest('.page').find('a.nr.selected');
+            alert('zet nr uit: delete uit balie pagina');
+            if (selectedPas.closest('.row').find('.col').length === 1) {
+                //only 1 col left in this row, so delete row with the remaining selected col
+                selectedPas.closest('.row').remove();
+            } else {
+                //delete the col
+                selectedPas.closest('.col').remove();
+            }
+            var changeNav = $(this).closest('.page').find('nav.change');
+            var actionsNav = $(this).closest('.page').find('nav.actions');
+            actionsNav.show();
+            changeNav.hide();
+        });
+
+        //all sliders
         $('.flexslider').flexslider({
             animation: "slide",
             animationSpeed: 1000,
             slideshowSpeed: 10000,
             directionNav: false,
             keyboard: false,
-            slideshow: false,
+            slideshow: true,
             start: function(slider) {
                 //alert(slider.count);
                 slider.find('.meter').animate({
@@ -43,6 +90,13 @@ $(document).ready(function() {
     });
 
 });
+
+//popup melding
+function meldingKeyPress()
+{
+    var s = $("#nieuw_tafelnummer").val();
+    $("#nieuw_tafelnummer_value").text(s);
+}
 
 //log messages to console when available
 function l(msg) {
